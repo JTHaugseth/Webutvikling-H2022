f51b6e963add98d5445d42ed89a4f512fec42de8
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-09-20 12:05:22 +0200

    lecture code session 7

---
+++ Lecture Code/session7-html-css-JavaScript-wrapup/scripts/food.js
@@ -0,0 +1,95 @@
+/*
+	Kaller på denne funksjonen for å få tak i filmene.
+	Denne filen skal ikke endres på.
+*/
+
+const getAllDishes = () => {
+	
+	return dishArray = [
+		{ 
+			id: 1,
+      		category: 1,
+			imageSrc: "pizza_1.jpg", 
+			price: 159,
+      		description: "American style pizza with beef, mozzarella and black olives",
+ 		},
+   		{ 
+			id: 2,
+      		category: 1,
+			imageSrc: "pizza_2.jpg", 
+			price: 179,
+      		description: "Freshly baked pizza with arugula, tomato, red onion and mozzarella",
+		},
+    	{ 
+			id: 3,
+      		category: 1,
+			imageSrc: "pizza_3.jpg", 
+			price: 189,
+      		description: "homemade pizza made with salami, mozzarella and cheese, mushrooms",
+		},
+    	{ 
+			id: 4,
+      		category: 1,
+			imageSrc: "pizza_4.jpg", 
+			price: 139,
+      		description: "classic piza made with ham, black olives and green bell peppe",
+		},
+    	{ 
+			id: 5,
+      		category: 1,
+			imageSrc: "pizza_5.jpg", 
+			price: 209,
+      		description: "Ham, marinated aroma mushrooms and oregano",
+		},
+    	{ 
+			id: 6,
+      		category: 1,
+			imageSrc: "pizza_6.jpg", 
+			price: 159,
+      		description: "pizza with arugula and cherry tomatoes",
+		},
+    	{ 
+			id: 7,
+      		category: 2,
+			imageSrc: "pasta1.jpg", 
+			price: 159,
+      		description: "creamy garlic seafood pasta, with shrimp, calamari, squid heads and mussels",
+		},
+    	{ 
+			id: 8,
+      		category: 2,
+			imageSrc: "pasta2.jpg", 
+			price: 179,
+      		description: "creamy garlic seafood spaghetti with pesto salmon and spinach",
+		},
+   	 	{ 
+			id: 9,
+      		category: 2,
+			imageSrc: "pasta3.jpg", 
+			price: 189,
+      		description: "homemade creamy salmon spaghetti with tomato sauce",
+		},
+    	{ 
+			id: 10,
+      		category:2,
+			imageSrc: "pasta4.jpg", 
+			price: 139,
+      		description: "fresh penne spaghetti with tomato and basil leaves",
+		},
+    	{ 
+			id: 11,
+      		category: 2,
+			imageSrc: "pasta5.jpg", 
+			price: 209,
+      		description: "special homemade pasta baked with meat fillings",
+		},
+    	{ 
+			id: 11,
+      		category: 2,
+			imageSrc: "pasta6.jpg", 
+			price: 159,
+      		description: "pasta with chicken and mushroom",
+		}
+	];
+	
+}
