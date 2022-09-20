645bbd66d9830a5c16d0a794821076a69a35ab1c
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-09-20 17:36:40 +0200

    game

--- Prosjekter/Game/script.js
+++ Prosjekter/Game/script.js
@@ -2,14 +2,14 @@ let square = document.querySelector('.div');
 let sword = document.querySelector('.sword');
 let enemy = document.querySelector('.enemy');
 let enemyHealthbar = document.getElementsByClassName("healthbar");
-let moveBy = 20;
-
+let damageNumber = document.getElementsByClassName("damageNumber");
+let moveBy = 30;
+let enemyHealth = 90;
 
 window.addEventListener('load', () => {
     square.style.position = 'absolute';
-    square.style.left = 0;
-    square.style.top = 0;
-    enemyHealthbar.style.width = 90 +'px';
+    square.style.left = 50 + 'px';
+    square.style.top = 400 + 'px';
 });
 
 window.addEventListener('keydown', (e) => {
@@ -31,25 +31,44 @@ window.addEventListener('keydown', (e) => {
 window.addEventListener('keydown', (e) => {
     switch(e.code) {
         case 'Space':
-            sword.style = 'transform: rotate(120deg)';
-            setTimeout(swordUp, 300);
-            if (overlaps(sword, enemy) == true) {
-                enemyHealthbar.style.width = parseInt(enemyHealthbar.style.width) - moveBy + 'px';
-            }
+        attack();
     }
 });
 function swordUp() {
     sword.style = 'transform: rotate(40deg)';
 }
+function damageNumberRemove() {
+    damageNumber[0].removeChild(damageNumber[0].firstElementChild);
+}
+function attack() {
+    sword.style = 'transform: rotate(120deg)';
+            setTimeout(swordUp, 300);
+            if (overlaps(sword, enemy) == true) {
+                enemyHealth -= damage();
+                enemyHealthbar[0].style.width = enemyHealth + 'px';
+                damageNumber[0].innerHTML += `<p>${damage()}</p>`;
+                setTimeout(damageNumberRemove, 500);
+            }
+            if (enemyHealth <= 0) {
+                enemy.style.display = 'none';
+            }
+}
+
 function overlaps(a, b) {
     const rect1 = a.getBoundingClientRect();
     const rect2 = b.getBoundingClientRect();
+    
     const isInHoriztonalBounds =
       rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x;
-    const isInVerticalBounds =
+    
+      const isInVerticalBounds =
       rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y;
-    const isOverlapping = isInHoriztonalBounds && isInVerticalBounds;
-    console.log(isOverlapping);
+    
+      const isOverlapping = isInHoriztonalBounds && isInVerticalBounds;
+    
     return isOverlapping;
+}
+function damage() {
+    return Math.floor(Math.random() * 7) + 5;
 }
 
