4aa64a483b23a7baec4c2009a8f757a0aaa22e63
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-11-07 12:52:33 +0100

    Update

--- Arbeidskrav/scripts/players.js
+++ Arbeidskrav/scripts/players.js
@@ -1,87 +1,88 @@
-
-let retrieveAllPlayers = ( function() {
-
-    const playersArray = [
+// Starting players.
+const playersArray = [
     {
-        id: 1,
         name: 'Haaland',
         rating: 99,
-        imgSrc: './images/haaland.webp',
+        imgSrc: './images/haaland.png',
         origin: 'Norway',
         league: 'Premier League',
         club: 'Manchester City',
     },
     {
-        id: 2,
         name: 'Richarlison',
         rating: 97,
-        imgSrc: './images/richarlison.webp',
+        imgSrc: './images/richarlison.png',
         origin: 'Brazil',
         league: 'Premier League',
         club: 'Spurs',
     },
     {
-        id: 3,
         name: 'Koulibaly',
         rating: 98,
-        imgSrc: './images/koulibaly.webp',
+        imgSrc: './images/koulibaly.png',
         origin: 'Senegal',
         league: 'Premier League',
         club: 'Chelsea',
     },
     {
-        id: 4,
         name: 'Ketelaere',
         rating: 96,
-        imgSrc: './images/ketelaere.webp',
+        imgSrc: './images/ketelaere.png',
         origin: 'Belgium',
         league: 'Serie A TIM',
         club: 'Milan',
     },
     {
-        id: 5,
         name: 'Ronaldo',
         rating: 96,
-        imgSrc: './images/ronaldo.webp',
+        imgSrc: './images/ronaldo.png',
         origin: 'Portugal',
         league: 'Premier League',
         club: 'Manchester United',
     },
     {
-        id: 6,
         name: 'Messi',
         rating: 99,
-        imgSrc: './images/messi.webp',
+        imgSrc: './images/messi.png',
         origin: 'Argentina',
         league: 'Champions League',
         club: 'PSG',
     },
     {
-        id: 7,
         name: 'Sterling',
         rating: 84,
-        imgSrc: './images/sterling.webp',
+        imgSrc: './images/sterling.png',
         origin: 'England',
         league: 'Premier League',
         club: 'Chelsea',
     },
     {
-        id: 8,
         name: 'Zlatan',
         rating: 82,
-        imgSrc: './images/zlatan.jpg',
+        imgSrc: './images/zlatan.png',
         origin: 'Sweden',
         league: 'Serie A TIM',
         club: 'Milan',
     },
 ];
 
+// If localStorage is empty, we add our own players from the array above.
+if (localStorage.getItem("Players")) {
+    console.log("Localstorage exists");
+} else {
+    localStorage.setItem("Players", JSON.stringify(playersArray));
+}
+
+// The function under exports our localStorage to other scripts in our file.  
+let retrieveAllPlayers = ( function() {
+    const storedArray = JSON.parse(localStorage.getItem("Players"));
+
 const getAllPlayers = () => {
-    return playersArray;
+    return storedArray;
 }
 
 return{getAllPlayers}
-
 }());
 
+//exports players module to other js scripts.
 export default retrieveAllPlayers;
--- Arbeidskrav/scripts/showPlayers.js
+++ Arbeidskrav/scripts/showPlayers.js
@@ -1,43 +1,28 @@
 import retrieveAllPlayers from "./players.js"
 
+const outputMain = document.getElementById('output-main');
+const players = retrieveAllPlayers.getAllPlayers();
 
-    // Get elements
-    const outputMain = document.getElementById('Output-main');
-    const players = retrieveAllPlayers.getAllPlayers();
-
-//  // Delete players
-// //  const deletePlayer =() => {
-// //     let id = parseInt(deleteIdTxt.value);
-// //     let index = movies.findIndex(player=>player.id===id);
-// //     players.splice(index, 1);
-// //     showPlayers(players);
-//  }
- 
- 
- 
-   
-
- const printAllPlayers = (players)=>{
+const printAllPlayers = (players)=>{
     
     let htmlTxt = "";
 
     players.forEach(player => {
-        htmlTxt += `<article>
-                <div>
-                    <h2>${player.name}</h2>
-                    <img class="images" src="${player.imgSrc}" alt= "Picture of: ${player.name}"></img>
-                    <h4 id="playerRating">${player.rating}<h4>
-                    <p id="playerOrigin">${player.origin}<p>
-                    <p id="playerLeauge">${player.league}<p>
-                    <p id="playerClub">${player.club}<p>
-                </div>
-             </article>`;
+        htmlTxt += `<article class="playerCard">
+                        <div>
+                            <h2>${player.name}</h2>
+                            <img class="imageSize" src="${player.imgSrc}" alt= "Picture of: ${player.name}">
+                            <p>Rating: ${player.rating}</p>
+                            <p>${player.origin}</p>
+                            <p>${player.league}</p>
+                            <p>${player.club}</p>
+                        </div>
+                    </article>`;
     });
-    console.log(htmlTxt);
     outputMain.innerHTML = htmlTxt;
 
  }
-
+ 
  (
     ()=>{
         printAllPlayers(players)
